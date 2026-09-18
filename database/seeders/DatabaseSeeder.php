36f34b6d1e588f1353651a4d237ce241d40edcdb
yanisparra1 <yanisparraq@gmail.com>
2026-09-17 20:47:37 -0400

    fix seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,10 +17,7 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+
 
         $this->call([
             ProductionDataSeeder::class,
