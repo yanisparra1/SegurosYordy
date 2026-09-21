e9b8360d88bd04f8dc59f637b847e4c0bf7b79a5
yanisparra1 <yanisparraq@gmail.com>
2026-09-21 12:00:34 -0400

    fix despues de registras en seguros

--- app/Filament/Resources/Seguros/Pages/CreateSeguro.php
+++ app/Filament/Resources/Seguros/Pages/CreateSeguro.php
@@ -18,4 +18,9 @@ class CreateSeguro extends CreateRecord
 
         return $data;
     }
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
 }
